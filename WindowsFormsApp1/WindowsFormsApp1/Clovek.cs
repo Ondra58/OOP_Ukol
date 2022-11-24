4d159aef4b0c9301a8d23a373abd70457516f32c
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2022-11-24 19:02:03 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Clovek.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Clovek
+    {
+        string jmeno;
+        DateTime narozen;
+        public Clovek(string jmeno, string narozen)
+        {
+            this.jmeno = jmeno;
+            this.narozen = DateTime.Parse(narozen);
+        }
+        public void NastavJmeno(string jmeno)
+        {
+            this.jmeno = jmeno;
+        }
+        public string VypisJmeno()
+        {
+            return jmeno;
+        }
+        public double Vek()
+        {
+            TimeSpan result = DateTime.Now - narozen;
+            return result.Days / 365.25;
+        }
+        public bool Plnolety()
+        {
+            if (Vek() >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }         
+        }
+        public string Starsi(Clovek dva)
+        {
+            if (dva.Vek() > this.Vek())
+            {
+                return dva.VypisJmeno() + " je starší než " + this.VypisJmeno();
+            }
+            else if(dva.Vek() < this.Vek())
+            {
+                return this.VypisJmeno() + " je starší než " + dva.VypisJmeno();
+            }
+            else
+            {
+                return this.VypisJmeno() + " je stejně starý jako " + dva.VypisJmeno();
+            }
+        }
+        public override string ToString()
+        {
+            if(Plnolety() == true)
+            {
+                return this.VypisJmeno() + " má " + (int)Vek() + " a je plnoletý. ";
+            }
+            else
+            {
+                return this.VypisJmeno() + " má " + (int)Vek() + " a není plnoletý. ";
+            }
+            
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clovek jedna = new Clovek(textBox1.Text, maskedTextBox1.Text);
+                Clovek dva = new Clovek(textBox2.Text, maskedTextBox2.Text);
+                MessageBox.Show(jedna.ToString());
+                MessageBox.Show(dva.ToString());
+                MessageBox.Show(jedna.Starsi(dva));
+            }
+            catch
+            {
+                MessageBox.Show("Chyba");
+            }
+            }
+    }
+}
